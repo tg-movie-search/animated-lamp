c79bb4239d5a7fb197aef26c3f005f57fad42e9f
tg-movie-search <110478235+tg-movie-search@users.noreply.github.com>
2026-06-02 15:45:40 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,13 +3,13 @@ import os
 
 class Config:
 
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    API_ID = int(os.environ.get("11333800"))
+    API_HASH = os.environ.get("e1b9a48abe756df09de3cc456975b481")
+    BOT_TOKEN = os.environ.get("8341647231:AAHdL0nJoSD_IBzQIZOvIojrId802t4SzMs")
     SESSION_NAME = os.environ.get("SESSION_NAME", ":memory:")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
-    DATABASE_URL = os.environ.get("DATABASE_URL")
-    AUTH_USERS = [int(i) for i in os.environ.get("AUTH_USERS", "").split(" ")]
+    LOG_CHANNEL = int(os.environ.get("-1002410328884"))
+    DATABASE_URL = os.environ.get("mongodb+srv://Strange:Strange@cluster0.wpvqktw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    AUTH_USERS = [int(i) for i in os.environ.get("AUTH_USERS", "6505906100").split(" ")]
     MAX_PROCESSES_PER_USER = int(os.environ.get("MAX_PROCESSES_PER_USER", 2))
     MAX_TRIM_DURATION = int(os.environ.get("MAX_TRIM_DURATION", 600))
     TRACK_CHANNEL = int(os.environ.get("TRACK_CHANNEL", False))
